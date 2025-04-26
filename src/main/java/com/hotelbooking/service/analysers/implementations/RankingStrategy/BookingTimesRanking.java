b1386bd5f3d05df8b40b599cefde32e53672ce3f
Manuel-F-04 <manuel_fam_franz@yahoo.de>
2025-04-26 22:38:51 +0200

    Refactor BookingTimesRanking.java

--- src/main/java/com/hotelbooking/service/analysers/implementations/RankingStrategy/BookingTimesRanking.java
+++ src/main/java/com/hotelbooking/service/analysers/implementations/RankingStrategy/BookingTimesRanking.java
@@ -7,12 +7,21 @@ import java.time.LocalDateTime;
 public class BookingTimesRanking implements IActivityRankingStrategy
 {
     @Override
-    public double getActivityRankingValue(Activity activity)
-    {
-        long bookingsLastYear = activity.getActivity_users().stream().filter(activityUser ->
-                activityUser.getStartDateTime().isAfter(LocalDateTime.now().minusYears(1))).count();
-        long bookingsLastMonth = activity.getActivity_users().stream().filter(activityUser ->
-                activityUser.getStartDateTime().isAfter(LocalDateTime.now().minusMonths(1))).count();
-        return bookingsLastYear + bookingsLastMonth * 2;
+    public double getActivityRankingValue(Activity activity) {
+        return countBookingsLastYear(activity) + countBookingsLastMonth(activity) * 2;
+    }
+
+    private long countBookingsLastYear(Activity activity) {
+        return countBookingsSince(activity, LocalDateTime.now().minusYears(1));
+    }
+
+    private long countBookingsLastMonth(Activity activity) {
+        return countBookingsSince(activity, LocalDateTime.now().minusMonths(1));
+    }
+
+    private long countBookingsSince(Activity activity, LocalDateTime since) {
+        return activity.getActivity_users().stream()
+                .filter(activityUser -> activityUser.getStartDateTime().isAfter(since))
+                .count();
     }
 }
